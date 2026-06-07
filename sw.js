676608fb94eb461b04cdba540747a6729bf347a8
abomagdy715-cmd <abomagdy715@gmail.com>
2026-06-07 08:11:53 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE = 'mezaniyati-v1';
+const ASSETS = [
+  '/Mezanya/',
+  '/Mezanya/index.html',
+  '/Mezanya/manifest.json'
+];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE).then(function(c) { return c.addAll(ASSETS); })
+  );
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(r) {
+      return r || fetch(e.request);
+    })
+  );
+});
